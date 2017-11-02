4b76cd1f1798a28cbb38f7b81cf11aa42c2d9fa8
haintwork <haint.work@gmail.com>
2017-11-02 21:48:34 +0700

    init project

---
+++ src/main/java/com/main/HelloGradle.java
@@ -0,0 +1,10 @@
+package com.main;
+
+public class HelloGradle {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle!!!");
+    }
+}
